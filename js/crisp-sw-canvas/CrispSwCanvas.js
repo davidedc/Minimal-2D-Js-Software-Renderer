044aef2f28b1a71b4cc3753be5bf768cd113526b
Davide Della Casa <davidedc@gmail.com>
2024-12-18 21:10:52 +0000

    sw context is immediately created at construction of sw canvas

--- js/crisp-sw-canvas/CrispSwCanvas.js
+++ js/crisp-sw-canvas/CrispSwCanvas.js
@@ -8,18 +8,21 @@ class CrispSwCanvas {
         height = aHeight;
         frameBuffer = new Uint8ClampedArray(aWidth * aHeight * 4).fill(0);
         this.frameBuffer = frameBuffer;
+        
+        // Create the context immediately and store it privately
+        this._context = new CrispSwContext(this);
     }
 
     getContext(contextType) {
         if (contextType !== "2d") {
             throw new Error("Only '2d' context is supported");
         }
-        return new CrispSwContext(this);
     }
 
     blitToCanvas(canvas) {
         const ctx = canvas.getContext('2d');
         const imageData = new ImageData(this.frameBuffer, this.width, this.height);
         ctx.putImageData(imageData, 0, 0);
+        return this._context;
     }
 }
