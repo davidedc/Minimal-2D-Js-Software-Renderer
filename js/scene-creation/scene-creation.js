eed9ac3ed52f450385b441e69ccf6c6d41f96226
Davide Della Casa <davidedc@gmail.com>
2025-01-25 21:53:54 +0000

    fixing the buildScene function

--- js/scene-creation/scene-creation.js
+++ js/scene-creation/scene-creation.js
@@ -6,14 +6,14 @@ function checkCanvasHasEvenDimensions() {
 
 function buildScene(shapes, log) {
   addRandomLines(shapes, log, 15);
-  addAxisAlignedRectangles(shapes, log, 5); // TODO needs to be fixed
+  addAxisAlignedRectangles(shapes, log, 5); // fine
   addRotatedRectangles(shapes, log, 5); // fine
   addAxisAlignedRoundedRectangles(shapes, log, 10); // fine
-  addLargeTransparentRoundedRectangles(shapes, log, 10); // TODO needs tweaking of centers to get strokes to be crisp
-  addNoStrokeRoundedRectangles(shapes, log, 10); // fine because there is no stroke, the fills positions are handled by getCornerBasedRepresentation, which always forces the corner to be at a grid, and the width and height are integers so everything is OK.
+  addLargeTransparentRoundedRectangles(shapes, log, 10); // fine
+  addNoStrokeRoundedRectangles(shapes, log, 10); // fine because there is no stroke, the fills positions are handled by getRectangularFillGeometry, which always forces the corner to be at a grid, and the width and height are integers so everything is OK.
   // addRotatedRoundedRectangles(shapes, log, 3); // TODO completely broken, "drawArcSWHelper" function is missing.
   addNinetyDegreeArcs(shapes, log); // roughly fine.
   addRandomArcs(shapes, log, 3); // roughly fine.
   addRandomCircles(shapes, log, 5); // looks pretty horrible, but roughly fine.
-  addThinStrokeRoundedRectangles(shapes, log, 10); // fine
+  addThinOpaqueStrokeRoundedRectangles(shapes, log, 10); // fine
 }
