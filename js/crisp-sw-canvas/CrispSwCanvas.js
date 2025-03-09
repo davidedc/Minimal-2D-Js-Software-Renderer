634b2d9a7667cc375d1f569866bdf576ad0c841a
Davide Della Casa <davidedc@gmail.com>
2025-03-09 20:04:58 +0000

    constructor of CrispSwCanvas can now also be passed a Canvas

--- js/crisp-sw-canvas/CrispSwCanvas.js
+++ js/crisp-sw-canvas/CrispSwCanvas.js
@@ -3,8 +3,17 @@ class CrispSwCanvas {
     static version = '1.0.2';
 
     constructor(width, height) {
-        this.width = width;
-        this.height = height;
+        // Support both (width, height) and (canvas) constructor styles
+        if (typeof width === 'object') {
+            const canvas = width;
+            this.width = canvas.width;
+            this.height = canvas.height;
+            this.title = canvas.title || '';
+        } else {
+            this.width = width;
+            this.height = height;
+            this.title = '';
+        }
         
         // Create the context immediately and store it privately
         this._context = new CrispSwContext(this);
